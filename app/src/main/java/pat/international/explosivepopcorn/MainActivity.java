8fa2cd4d0579eb27c32c752baada9e93d4eff83a
patocastillo <patricio.castillo2009@gmail.com>
2023-03-11 10:21:53 +0100

    Added layouts for categories

--- app/src/main/java/pat/international/explosivepopcorn/MainActivity.java
+++ app/src/main/java/pat/international/explosivepopcorn/MainActivity.java
@@ -37,8 +37,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+/*        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
 
 
     }
